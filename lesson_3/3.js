f8914dd6a54f9262b548580baa288bffb2f034e6
Anvich <53406163+Anvich@users.noreply.github.com>
2020-08-05 13:38:27 +0300

    Create 3.js

---
+++ lesson_3/3.js
@@ -0,0 +1,13 @@
+function MinMax(a, b){
+    var a,b;
+    if(a>b){
+        console.log("Наибольшее число "+ a)
+    }else if(b>a){
+         console.log("Наибольшее число "+ b)
+    }else {
+        console.log("Значения равны!")
+    }
+}
+MinMax(3,7)
+MinMax(10,4)
+MinMax(5,5)
