c9548ddfc4c1114cc94a0a43168eabf6e5077ce6
Anvich <53406163+Anvich@users.noreply.github.com>
2020-08-05 13:29:34 +0300

    Create 2.js

---
+++ lesson_3/2.js
@@ -0,0 +1,11 @@
+let arr = [5, 4, 3, 8, 0], mas=[], a, n=0;
+function  filterFor(arr, a){
+    for(i=0;i<arr.length;i++){
+        if(arr[i]>=a){
+            mas[n]=arr[i]
+            n++
+        }
+    }
+    return mas
+}
+filterFor(arr,5)
