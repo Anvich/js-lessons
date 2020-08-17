6d7198cb224a397e671c0b4591574d9b2787c9ea
Anvich <53406163+Anvich@users.noreply.github.com>
2020-08-17 10:22:14 +0300

    Create script.js

---
+++ lesson_4/2/script.js
@@ -0,0 +1,5 @@
+let div=document.querySelector('div')
+let p=document.createElement('p')
+p.innerText='… Политика интересует меня только по одной причине — чтобы дожить до того дня, когда мне уже не нужно будет интересоваться политикой…'
+
+div.appendChild(p)
